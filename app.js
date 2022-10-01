14d9aa1d744aadc157d655bb9082502764cedd8d
ThaiTuanBTT <tuanbtgch200717@fpt.edu.vn>
2022-10-01 17:31:54 +0700

    ok

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+//module
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000
 const path = require("path");
 
+//database connect + access
 const mongoose = require("mongoose");
 const studentModel = require("./models/StudentSchema");
 const url = "mongodb://localhost:27017/greenwich";
@@ -12,6 +14,41 @@ mongoose.connect(url, { useNewUrlParser: true }, (err) => {
     } else {
         console.log("connect to db success !");
     }
+});
+
+//body-parser: get input from FORM
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//render ra form nhập liệu
+app.get("/add", (req, res) => {
+    res.render("add")
+});
+
+//nhận và xử lý dữ liệu từ form add
+app.post("/add", (req, res) => {
+    //console.log(req.body);
+    //res.send(req.body);
+    // res.render("output", {
+    //     student: req.body
+    // });
+    //tạo object student chứa dữ liệu nhập từ form
+    var student = new studentModel({
+        name: req.body.name,
+        age: req.body.age,
+        email: req.body.email,
+        image: req.body.image,
+    });
+    //lưu object student vào database
+    student.save((err) => {
+        if (err) {
+            if (err) {
+                console.error(err);
+            } else {
+                res.redirect("/student")
+            }
+        }
+    })
 });
 
 app.set("views", path.join(__dirname, "views"));
@@ -24,6 +61,21 @@ app.get("/", (req, res) => {
 //req: nhập liệu từ người dùng
 app.get("/about", function(req, res) {
     res.render("about");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.post("/login", (req, res) => {
+    var username = req.body.username;
+    var password = req.body.password;
+    var check = false;
+    if (username == "admin" && password == "123456") {
+        check = "Login succed !";
+    }
+    //res.send(check)
+    res.render("check", { result: check });
 });
 
 app.get("/student", (req, res) => {
